c206722fcd4fb0d2ab435f1daa7bce223a613442
sesareo <sesareo.code@outlook.com>
2023-05-09 15:44:32 -0400

    cambio componente notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,8 +7,8 @@ const NotFound = () => {
       {/* <img src="src/assets/img/error-404-adobe-3.jpg" alt="error-404" /> */}
       <img src="assets/img/error-404-adobe-2.jpg" alt="error-404" />
       <p className="not-found-texto">¿Qué pachó?</p>
-      <p className="not-found-texto">Ch bah puta la güeá</p>
-      <p className="not-found-texto">Esta página no existe</p>
+      <p className="not-found-texto">Ch bah puta la güeá, esta página no existe</p>
+      {/* <p className="not-found-texto">Esta página no existe</p> */}
       <div>
         {/* <{p>NotFound: Error 404</p>} */}
         {/* <p className="not-found-texto">Lo siento, esta página no existe</p> */}
