ef06c8bf23136eba9b32f4b1a031141d96ac4829
sesareo <sesareo.code@outlook.com>
2023-05-09 15:42:35 -0400

    cambio carpeta img a public

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,8 +4,11 @@ import Container from 'react-bootstrap/Container';
 const NotFound = () => {
   return (
     <div className="not-found">
-      <img src="src/assets/img/error-404-adobe-3.jpg" alt="error-404" />
-      <p className="not-found-texto">Lo sentimos, esta página no existe</p>
+      {/* <img src="src/assets/img/error-404-adobe-3.jpg" alt="error-404" /> */}
+      <img src="assets/img/error-404-adobe-2.jpg" alt="error-404" />
+      <p className="not-found-texto">¿Qué pachó?</p>
+      <p className="not-found-texto">Ch bah puta la güeá</p>
+      <p className="not-found-texto">Esta página no existe</p>
       <div>
         {/* <{p>NotFound: Error 404</p>} */}
         {/* <p className="not-found-texto">Lo siento, esta página no existe</p> */}
