7194ddc17d55ef985f64f6009e44f415864cd4c5
sesareo <sesareo.code@outlook.com>
2023-05-09 00:31:01 -0400

    nuevos nuevos cambios en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  root: './',
-    build: {
-        outDir: 'dist',
-    },
-    publicDir: 'assets'
+  build: {
+    outDir: 'assets',
+  },
 })
